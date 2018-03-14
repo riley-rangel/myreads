a3ed8b70d64c2e96482d1d83801ebe2bd29ec9e6
Riley Rangel <rrangel91@gmail.com>
2018-03-13 22:30:28 -0700

    refactor: Refactor Search component to hold state in App component. #13

--- src/Search.js
+++ src/Search.js
@@ -1,57 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
 import Book from './Book'
 
-export default class Search extends Component {
-  state = {
-    query: '',
-    books: []
-  }
-
-  handleChange = query => {
-    this.setState({ query })
-    this.submitQuery(query)
-  }
-  submitQuery = query => {
-    if (!query) return this.setState({ books: [] })
-
-    BooksAPI.search(query)
-      .then(books => {
-        return Array.isArray(books)
-          ? this.setState({ books })
-          : this.setState({ books: [] })
-      })
-      .catch(err => console.error(err))
-  }
-  render () {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to='/' className="close-search">
-            Close
-          </Link>
-          <div className="search-books-input-wrapper">
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              value={this.state.query}
-              onChange={({ target }) => this.handleChange(target.value)}
-            />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
-            {this.state.books.map(book => {
-              return <Book
-                key={book.id}
-                book={book}
-                onSelect={this.props.onSelect}
-              />
-            })}
-          </ol>
+ const Search = ({books, query, onSearch, onSelect}) => {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link to='/' className="close-search">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            value={query}
+            onChange={({ target }) => onSearch(target.value)}
+          />
         </div>
       </div>
-    )
-  }
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {books.map(book => {
+            return <Book
+              key={book.id}
+              book={book}
+              onSelect={onSelect}
+            />
+          })}
+        </ol>
+      </div>
+    </div>
+  )
 }
+
+export default Search
