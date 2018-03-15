74b64b291db703fd53e7cb3a6162058381c7cf4f
Riley Rangel <rrangel91@gmail.com>
2018-03-14 19:04:54 -0700

    refactor: Making requested changes per code review one. #15

---
+++ src/shelfConfig.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 'currentlyReading',
+    title: 'Currently Reading'
+  },
+  {
+    id: 'wantToRead',
+    title: 'Want to Read'
+  },
+  {
+    id: 'read',
+    title: 'Read'
+  }
+]
